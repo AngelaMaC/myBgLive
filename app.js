aadb80aeb06b1c5ea62cfb8acf2f40ab6d416235
Angela Canning <46754565+AngelaMaC@users.noreply.github.com>
2022-08-30 17:24:20 -0500

    Chartjs and auto update

--- app.js
+++ app.js
@@ -1,88 +1,122 @@
-// const dateData = [];
-// const sgvData = [];
+refreshData()
 
-// async function getData() {
-//     const url = 'https://canning.herokuapp.com/api/v1/entries/sgv.json?find[dateString][$gte]=2022-05-01&find[dateString][$lte]=2022-05-02&count=10';
-//     const response = await fetch(url);
-//     const data = await response.json();
+async function refreshData() {
+    // Get BG data
+    async function getData() {
+        const url = 'https://canning.herokuapp.com/api/v1/entries/sgv.json?count=125'
+        const response = await fetch(url)
+        const datapoints = await response.json()
+        // reverse order of data so chart displays from left to right
+        return datapoints.reverse()
+    }
 
+    // Map through data and pull out only bg (sgv) and date (datestring)
+    getData().then(datapoints => {
+        const date = datapoints.map(datapoint => {
+            return new Date(datapoint.dateString).toLocaleString([], { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' })
+        })
+        const sgv = datapoints.map(datapoint => {
+            return datapoint.sgv
+        })
 
+        // chart setup        
 
-//     const dateTimeMap = data.map(
-//         function (index) {
-//             return new Date(index.dateString);
-//         });
+        const labels = date
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'BG Data',
+                data: sgv, // result of sgv map
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                tension: 0.4
+            }]
+        }
 
+        // chart config
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                layout: {
+                    padding: 20
+                },
+                responsive: true,
+                scales: {
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'BG',
+                            font: {
+                                size: 17
+                            },
+                        },
+                        min: 40,
+                        max: 300,
+                        ticks: {
+                            stepSize: 20
+                        },
+                        beginAtZero: false
+                    },
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Time',
+                            font: {
+                                size: 17
+                            },
+                            type: 'time',
+                            time: {
+                                unit: 'day'
+                            },
+                        },
+                        ticks: {
+                            // display only time in labels, but full data on datapoint hover
+                            callback: function (value) {
+                                return this.getLabelForValue(value).substr(-8);
+                            }
+                        },
+                    }
+                }
+            }
+        }
 
-//     dateData.push(dateTimeMap.toLocaleString('en-US', { hour12: false }));
+        // render chart
+        let myChart = null
 
-//     const sgvMap = data.map(
-//         function (index) {
-//             return index.sgv;
-//         });
+        if (Chart.getChart('myChart')) {
+            Chart.getChart('myChart').destroy();
+        }
 
-//     sgvData.push(sgvMap);
+        myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        )
 
-//     document.querySelector('#get-date').insertAdjacentHTML = data.dateTimeMap;
-
-//     return data;
-// }
-
-// getData();
-// console.log(dateData);
-// console.log(sgvData);
-
-
-const sgvData = [];
-const dateData = [];
-const globalData = [];
-
-// document.querySelector('.app').insertAdjacentHTML('beforeend', /*html*/`
-//     <div>
-//     <h1>My BG's</h1>
-//     <div id='data'>${getData()}</div>
-//     </div>
-// `);
-
-const date = document.querySelector('#get-date');
-date.insertAdjacentHTML('beforeend', /*html*/`
-    <div>${globalData}</div>
-`);
-
-
-async function getData() {
-    const url = 'https://canning.herokuapp.com/api/v1/entries/sgv.json?find[dateString][$gte]=2022-05-01&find[dateString][$lte]=2022-05-02&count=10';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-
-
-    const dateTimeMap = data.map(
-        function (index) {
-            //return index.dateString;
-            let date = new Date(index.dateString);
-            return date.toLocaleString();
-        });
-
-
-    dateData.push(dateTimeMap);
-
-    const sgvMap = data.map(
-        function (index) {
-            return index.sgv;
-        });
-
-    sgvData.push(sgvMap);
-
-
-    globalData.push({ dateTimeMap, sgvMap })
-    return data;
-
+        updateUser()
+    });
 }
-getData();
-document.querySelector('#get-date').innerText = globalData;
-console.log(sgvData);
-console.log(dateData);
-console.log(globalData);
+
+// auto fetch new BG data every five minutes
+setInterval(refreshData, 300000)
+
+// update message
+
+function updateUser() {
+    const updated = document.querySelector('#refresh-data')
+    const date2 = new Date().toLocaleString([], { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' })
+
+    updated.innerHTML = `<p id='data-update'>Updated on ${date2}</p>`
+}
 
 
+// optional manual button to refresh data
+// const btn = document.querySelector('#refresh-data')
+
+// btn.addEventListener('click', () => {
+//     refreshData()
+//     console.log('data refreshed')
+//     const updated = document.querySelector('#button-data');
+//     const date2 = new Date()
+//     updated.insertAdjacentHTML('beforebegin', `<p id='data-update'>Updated on ${date2.toLocaleString([], { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' })}</p>`);
+// })
